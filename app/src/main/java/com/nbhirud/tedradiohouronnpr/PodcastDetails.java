42096bd7eba91a3bb178939a98c69bde9cd99cad
Nikhil Bhirud <nbhirud@gmail.com>
2017-04-30 18:26:36 -0400

    v1.1

--- app/src/main/java/com/nbhirud/tedradiohouronnpr/PodcastDetails.java
+++ app/src/main/java/com/nbhirud/tedradiohouronnpr/PodcastDetails.java
@@ -1,10 +1,10 @@
-package com.nbhirud.hw4;
+package com.nbhirud.tedradiohouronnpr;
 
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
